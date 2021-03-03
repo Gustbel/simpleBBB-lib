0a6513a82ec4065c9052d0684e2e824f6b59432e
Gustavo Belbruno <gustavobelbruno@gmail.com>
2021-03-03 11:55:37 -0300

    fixed read gpio

--- src/gpio/gpio.c
+++ src/gpio/gpio.c
@@ -11,20 +11,18 @@ char* s;
 int f;
 char value;
 
-char* path(int gpio, char* file) 
+char* path(int gpio, char* file)
 {
-
   sprintf(aux_int, "%d", gpio); // Convert int to string
 
   strcpy(buffer, "/sys/class/gpio/gpio");	// Init the buffer path
   strcat(buffer, aux_int);
   strcat(buffer, "/");
   strcat(buffer, file);
-  
+
   s = " ";
   s=buffer;
   return s;	// Return the path
-
 }
 
 void set_GPIO_direction(int gpio, int d) //        0 - OUTPUT      ----      1 - INPUT
@@ -47,20 +45,20 @@ void set_GPIO_direction(int gpio, int d) //        0 - OUTPUT      ----      1 -
 void set_GPIO_value(int gpio, bool l) //        false - Low      ----      true - High
 {
 	f = open(path(gpio, "value"), O_RDWR);
-	
+
 	if (l)
     	write(f, "1", 1);
 	else
     	write(f, "0", 1);
-    	
+
     close(f);
 }
 
 int read_GPIO_value(int gpio) //        Read GPIO value   --   0 (Low) or 1 (High)
 {
 	f = open(path(gpio, "value"), O_RDONLY, 0);
-	
     read(f, &value, 1);
-	
-	return value;
+    close(f);
+
+	return (value - '0');
 }
--- src/main.c
+++ src/main.c
@@ -6,10 +6,9 @@
 #include "gpio/gpio.h"
 
 int main () {
-
 	set_GPIO_direction(60, 0);
 	printf ("set_direction GPIO 60 -> Output\n\n");
-	
+
 	for (int i=0; i<10; i++)
 	{
 		set_GPIO_value(60,true);
@@ -19,6 +18,6 @@ int main () {
 		read_GPIO_value(60);
 		usleep(1000000);
 	}
-	
+
 	return 0;
 }
